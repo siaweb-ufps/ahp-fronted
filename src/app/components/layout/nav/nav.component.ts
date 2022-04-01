3b3027dfaf010eb7f52dfc22beafcc21997478a1
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2022-04-01 18:28:14 -0500

    ajustes login

--- src/app/components/layout/nav/nav.component.ts
+++ src/app/components/layout/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowRightFromBracket, faChartColumn, faUser, faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faChartColumn, faRightToBracket, faUser, faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,15 +8,26 @@ import { faArrowRightFromBracket, faChartColumn, faUser, faXmarkSquare } from '@
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  isLogged:boolean = false;
   logo:any = './assets/images/min-logo-white.png';
 
-  constructor() { }
+  constructor(private tokenService: TokenService) { }
   
   ngOnInit(): void {
+    (this.tokenService.getToken()) 
+      ? this.isLogged = true 
+      : this.isLogged = false
   }
+
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
   faUser = faUser;
   faXmarkSquare = faXmarkSquare;
   faChartColumn = faChartColumn;
+  faRightToBracket = faRightToBracket;
   faArrowRightFromBracket = faArrowRightFromBracket;
 
 }
